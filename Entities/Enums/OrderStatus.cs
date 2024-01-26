a8a2f4995bf091f71ff761531817f9ab04ce2b9b
Isaac Vargas Vaccaro <contato.isaacvargas@gmail.com>
2024-01-26 15:54:07 -0300

    first commit

---
+++ Entities/Enums/OrderStatus.cs
@@ -0,0 +1,10 @@
+﻿namespace desafio_composicao2_csharp.Entities.Enums
+{
+    enum OrderStatus : int
+    {
+        PendingPayment = 0,
+        Processing = 1,
+        Shipped = 2,
+        Delivered = 3
+    }
+}
